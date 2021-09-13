676e1e38497c877ff1cbed5a2477418de65024eb
gauravjaglan <jaglan.gaurav@gmail.com>
2021-09-13 23:39:52 +0530

    Added Exception block

--- ethereum_crawler.py
+++ ethereum_crawler.py
@@ -7,17 +7,19 @@ def get_transaction_data(transaction_address, block_start, block_end, date):
     #&page=1&offset=10
     payload = ""
     headers = {}
-
-    response = requests.request("GET", url)
-    if response.status_code==200:
-        transactions = pandas.DataFrame(response.json()['result'])
-        transactions = transactions.loc[(transactions['timeStamp'] <= date)]
-        transactions.timeStamp = pandas.to_datetime(transactions['timeStamp'], unit='s')
-        transactions['gasPrice'] = transactions['gasPrice'].apply(float)
-        transactions['gasUsed'] = transactions['gasUsed'].apply(float)
-        transactions['ethereum consumed'] = (transactions['gasPrice'].apply(float)*transactions['gasUsed'].apply(float))/1000000000000000000
-    return transactions[['blockNumber','from','gas','gasPrice','ethereum consumed','timeStamp']]
-
+    try:
+        response = requests.request("GET", url)
+        if response.status_code==200:
+            transactions = pandas.DataFrame(response.json()['result'])
+            transactions = transactions.loc[(transactions['timeStamp'] <= date)]
+            transactions.timeStamp = pandas.to_datetime(transactions['timeStamp'], unit='s')
+            transactions['gasPrice'] = transactions['gasPrice'].apply(float)
+            transactions['gasUsed'] = transactions['gasUsed'].apply(float)
+            transactions['ethereum consumed'] = (transactions['gasPrice'].apply(float)*transactions['gasUsed'].apply(float))/1000000000000000000
+        return transactions[['blockNumber','from','gas','gasPrice','ethereum consumed','timeStamp']], 200
+    except Exception as e:
+        print(e)
+        return "",response.status_code
 
 get_transaction_data("0xaa7a9ca87d3694b5755f213b5d04094b8d0f0a6f",'9000000','9099999', '2021-12-13')
 
